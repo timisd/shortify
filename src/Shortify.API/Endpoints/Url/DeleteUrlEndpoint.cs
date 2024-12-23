7cbb1a639c957b93ad5089ed31f9738d6253330a
Tim <tim-gideon.quell@stud.hshl.de>
2024-12-23 19:28:14 +0100

    added endpoint for deleting url entry

---
+++ src/Shortify.API/Endpoints/Url/DeleteUrlEndpoint.cs
@@ -0,0 +1,40 @@
+using FastEndpoints;
+using Shortify.API.Contracts.Response;
+using Shortify.Persistence;
+
+namespace Shortify.API.Endpoints.Url;
+
+public class DeleteUrlEndpoint(IUrlRepository urlRepo) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Delete("/api/urls/{id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var id = Route<string>("id");
+        var isGuid = Guid.TryParse(id, out var guid);
+        if (!isGuid)
+        {
+            await SendAsync(new GetUrlResponse
+            {
+                Success = false,
+                Message = "Invalid id"
+            }, StatusCodes.Status400BadRequest, ct);
+            return;
+        }
+
+
+        var url = await urlRepo.DeleteUrlAsync(guid, ct);
+        if (url == null)
+            await SendAsync(new GetUrlResponse
+            {
+                Success = false,
+                Message = "Url not found"
+            }, StatusCodes.Status404NotFound, ct);
+        else
+            await SendNoContentAsync(ct);
+    }
+}
