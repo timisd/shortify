fca7b011675dd906de22cd135189118e0f30b20d
Tim <tim-gideon.quell@stud.hshl.de>
2024-12-30 09:42:56 +0100

    added validation to short url

--- src/Shortify.WebUI/Pages/Index.cshtml.cs
+++ src/Shortify.WebUI/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +10,10 @@ namespace Shortify.WebUI.Pages;
 
 public class IndexModel(ApiClient apiClient) : PageModel
 {
-    [BindProperty] public string OriginalUrl { get; set; } = string.Empty;
+    [BindProperty]
+    [RegularExpression(@"^(https?:\/\/)?(www\.)?([a-zA-Z0-9\-]+)(\.[a-zA-Z]{2,})(\/[^\s]*)?$",
+        ErrorMessage = "Please enter a valid URL ending with a domain.")]
+    public string OriginalUrl { get; set; } = string.Empty;
 
     [BindProperty] public string CustomShort { get; set; } = string.Empty;
 
